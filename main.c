90c432fedb12e1d9f4f23a75d5d332bc2e2b0a84
George Morgan <george@george-morgan.com>
2016-05-12 21:59:17 -0400

    3D cube projection and unilateral rotation. Ornthographic projection is incomplete.

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <SDL2/SDL.h>
 
-#define WIDTH 1920
-#define HEIGHT 1200
+#define WIDTH 640
+#define HEIGHT 480
 
 #define RED 255, 0, 0
 #define GREEN 0, 255, 0
 #define BLUE 0, 0, 255
 
-#define SDL_GetSurfacePixel(surface, x, y) (uint32_t *)(surface -> pixels + (y * surface -> pitch + (x * surface -> format -> BytesPerPixel)))
+#define LENGTH 100
+
+typedef struct _vertex {
+	float x, y, z;
+} vertex;
 
 void line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int r, int g, int b) {
 	
 	SDL_SetRenderDrawColor(renderer, r, g, b, 255);
 	SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
 	
+}
+
+float map(float x, float in_min, float in_max, float out_min, float out_max) {
+
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
 }
 
 int main(int argc, char *argv[]) {
@@ -26,7 +37,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Create an SDL window. */
-	SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 	if (!window) {
 		SDL_Quit();
 		return EXIT_FAILURE;
@@ -42,22 +53,6 @@ int main(int argc, char *argv[]) {
 
 	/* Clear the renderer. */
 	SDL_RenderClear(renderer);
-	
-	uint32_t rmask, gmask, bmask, amask;
-	#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	rmask = 0xff000000; gmask = 0x00ff0000; bmask = 0x0000ff00; amask = 0x000000ff;
-	#else
-	rmask = 0x000000ff; gmask = 0x0000ff00; bmask = 0x00ff0000; amask = 0xff000000;
-	#endif
-
-	/* Create an SDL surface. */
-	SDL_Surface *surface = SDL_CreateRGBSurface(0, WIDTH, HEIGHT, 32, rmask, gmask, bmask, amask);
-	if (!renderer) {
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
 
 	/* ~~~ APPLICATION BEGIN ~~~ */
 
@@ -97,7 +92,52 @@ int main(int argc, char *argv[]) {
 
 			/* ~~~ RENDERING BEGIN ~~~ */
 
-			line(renderer, 10, 10, 20, 20, GREEN);
+			vertex cube[] = {
+				{ 0, 0, 0 }, { 1, 0, 0 },
+				{ 1, 1, 0 }, { 0, 1, 0 },
+				{ 0, 0, 1 }, { 1, 0, 1 },
+				{ 1, 1, 1 }, { 0, 1, 1 },
+			};
+
+			/* Obtain the center of the screen, and use it as the origin. */
+			uint32_t cx = WIDTH / 2, cy = HEIGHT / 2;
+
+			/* Obtain the x and y location of the mouse. */
+			int mx, my;
+			SDL_GetMouseState(&mx, &my);
+
+			/* Map the mouse position to the input range of sin and cosine. */
+			float tx = map(mx, 0, WIDTH, -M_PI_2, M_PI_2);
+			float sin_tx = sin(tx), cos_tx = cos(tx);
+			float ty = map(my, 0, WIDTH, -1, 1);
+			float sin_ty = sin(ty), cos_ty = cos(ty);
+
+			/* Iterate through the verticies, transforming them one at a time. */
+			for (int v = 0; v < (sizeof(cube) / sizeof(vertex)); v ++) {
+				/* Make the edges longer. */
+				cube[v].x *= LENGTH, cube[v].y *= LENGTH, cube[v].z *= LENGTH;
+
+				/* Transform the X and Y coordinates to rotate around the Z axis. */
+				cube[v].x = cube[v].y * cos_tx - cube[v].x * sin_tx;
+				cube[v].y = cube[v].x * cos_tx + cube[v].y * sin_tx;
+
+				/* Transform the X and Y coordinates to rotate around the Y axis. */
+//				cube[v].x = cube[v].x * cos_ty - cube[v].z * sin_ty;
+//				cube[v].y = cube[v].z * cos_ty + cube[v].x * sin_ty;
+
+				/* Transform the X and Y coordinates to rotate around the Y axis. */
+				cube[v].x += cube[v].z;
+				cube[v].y += cube[v].z;
+			}
+
+			/* Draw the vertices. */
+			SDL_SetRenderDrawColor(renderer, GREEN, 255);
+			int total = (sizeof(cube) / sizeof(vertex));
+			for (int v = 0; v < total; v ++) {
+				for (int r = v; r < total; r ++){
+					line(renderer, cx + cube[v].x, cy + cube[v].y, cx + cube[r].x, cy + cube[r].y, GREEN);
+				}
+			}
 
 			/* ~~~ RENDERING END ~~~ */
 
@@ -112,7 +152,6 @@ int main(int argc, char *argv[]) {
 quit:
 
 	/* Clean up after ourselves by destroying that which we have created. */
-	SDL_FreeSurface(surface);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
